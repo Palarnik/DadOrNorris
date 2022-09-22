a681b0bc7d87bb956f8a1af16f7278c655b7a8d6
Maciej Mejer <maciej.mejer@op.pl>
2022-09-22 19:10:09 +0200

    Initial commit

---
+++ DadOrNorris/DadOrNorrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadOrNorrisApp.swift
+//  DadOrNorris
+//
+//  Created by Maciej Mejer on 22/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct DadOrNorrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
